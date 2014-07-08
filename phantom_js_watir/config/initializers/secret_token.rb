d318db1f15040d78d72bab40aa524eee4a31c767
Lakshmi Raghavan <rajulonline@gmail.com>
2014-07-07 17:06:25 -0700

    watir headless automation using phantom js

---
+++ phantom_js_watir/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhantomJsWatir::Application.config.secret_token = '71c33562b3031de72cae26ed7d6d533802774c0b221a7f179fe9966acf237db7e2ecb2acb3ab8dd678232501856d57a53bc57a390a843db6a9e1b1a1f009a4e5'
---
+++ phantom_js_watir/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PhantomJsWatir::Application.config.session_store :cookie_store, key: '_phantom_js_watir_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PhantomJsWatir::Application.config.session_store :active_record_store
